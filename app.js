fe7861981bcce06b4aea6ecf89e352895b992668
Anurag Bajaj <anuragb26@gmail.com>
2019-06-19 10:50:09 +0530

    Chore:Add Routes and nodemon

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+app.get("/", (req, res) => {
+  res.send("Index");
+});
+
+app.get("/about", (req, res) => {
+  res.send("about1");
+});
+
 app.listen(port, () => {
   console.log(`Server listening on ${port}`);
 });
