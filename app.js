c1791792cf81b0a114f1c0a76ded2e67ee16a52b
Anurag Bajaj <anuragb26@gmail.com>
2019-06-19 11:11:36 +0530

    Feat:Add Handlebars

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require("express");
+const exphbs = require("express-handlebars");
 const app = express();
+
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
 const port = 5000;
 
 app.get("/", (req, res) => {
-  res.send("Index");
+  res.render("index");
 });
 
 app.get("/about", (req, res) => {
-  res.send("about1");
+  const title = "Welcome";
+  res.render("about", { title });
 });
 
 app.listen(port, () => {
