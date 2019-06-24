f320b110f6104799b71ece37acdfd8e3b8631755
Anurag Bajaj <anuragb26@gmail.com>
2019-06-24 18:11:37 +0530

    Chore:Server Side Validation

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const app = express();
-
+app.use(bodyParser.urlencoded({ extended: false }));
 mongoose
   .connect(
     `mongodb+srv://anuragb26:anuragb26@cluster0-nx9b3.mongodb.net/test?retryWrites=true&w=majority`,
@@ -18,6 +19,10 @@ const Idea = mongoose.model("ideas");
 
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
+// app.use(express.json());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const port = 5000;
 
@@ -33,6 +38,25 @@ app.get("/about", (req, res) => {
 
 app.get("/ideas/add", (req, res) => {
   res.render("ideas/add");
+});
+
+app.post("/ideas", (req, res) => {
+  let errors = [];
+  if (!req.body.title) {
+    errors.push({ text: "Please add a title" });
+  }
+  if (!req.body.details.trim()) {
+    errors.push({ text: "Please add some details" });
+  }
+  if (errors.length > 0) {
+    res.render("ideas/add", {
+      errors,
+      title: req.body.title,
+      details: req.body.details
+    });
+  } else {
+    res.send("passed");
+  }
 });
 
 app.listen(port, () => {
