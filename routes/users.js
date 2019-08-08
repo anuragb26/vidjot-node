aff3d06b265fb33f6010d91688a72d5227d37de5
Anurag Bajaj <anuragb26@gmail.com>
2019-08-08 16:32:38 +0530

    Chore:Register Form Validation

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,20 @@ router.get("/register", (req, res) => {
   res.render("users/register");
 });
 
+router.post("/register", (req, res) => {
+  console.log(req.body);
+  const errors = [];
+  if (req.body.password !== req.body.password2) {
+    errors.push({ text: "Password do not match" });
+  }
+  if (req.body.password.length < 4) {
+    errors.push({ text: "Password must at least be 4 characters" });
+  }
+  if (errors.length > 0) {
+    res.render("users/register", { errors, ...req.body });
+  } else {
+    res.send("passed");
+  }
+});
+
 module.exports = router;
