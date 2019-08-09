51426dca285ba37122d73ecb1e3ac6613c0ccb74
Anurag Bajaj <anuragb26@gmail.com>
2019-08-09 15:38:53 +0530

    Chore:Save User

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
+const User = require("../models/User");
 const router = express.Router();
-
+const bcrypt = require("bcryptjs");
 router.get("/login", (req, res) => {
   res.render("users/login");
 });
@@ -21,7 +22,36 @@ router.post("/register", (req, res) => {
   if (errors.length > 0) {
     res.render("users/register", { errors, ...req.body });
   } else {
-    res.send("passed");
+    User.findOne({ email: req.body.email }).then(user => {
+      if (user) {
+        req.flash("error_msg", "Email already registered");
+        res.redirect("/users/register");
+      } else {
+        const newUser = new User({
+          name: req.body.name,
+          email: req.body.email,
+          password: req.body.password
+        });
+        bcrypt.genSalt(10, (err, salt) => {
+          bcrypt.hash(newUser.password, salt, (err, hash) => {
+            newUser.password = hash;
+            newUser
+              .save()
+              .then(user => {
+                req.flash(
+                  "success_msg",
+                  "You are now registered and can log in"
+                );
+                res.redirect("/users/login");
+              })
+              .catch(err => {
+                console.log(err);
+                return;
+              });
+          });
+        });
+      }
+    });
   }
 });
 
